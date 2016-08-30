f309bc8a14d000f3179785738b46e86de38d97f2
Josh Rathke <josh.rathke@ywammontana.org>
2016-08-30 00:23:34 -0600

    Initial Commit

---
+++ header.php
@@ -0,0 +1,71 @@
+<?php
+/**
+ * The template for displaying the header
+ *
+ * Displays all of the head element and everything up until the "container" div.
+ *
+ * @package FoundationPress
+ * @since FoundationPress 1.0.0
+ */
+
+?>
+<!doctype html>
+<html class="no-js" <?php language_attributes(); ?> >
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300,700" rel="stylesheet">
+		<?php wp_head(); ?>
+	</head>
+	<body <?php body_class(); ?>>
+	<?php do_action( 'foundationpress_after_body' ); ?>
+
+	<?php if ( get_theme_mod( 'wpt_mobile_menu_layout' ) == 'offcanvas' ) : ?>
+	<div class="off-canvas-wrapper">
+		<div class="off-canvas-wrapper-inner" data-off-canvas-wrapper>
+		<?php get_template_part( 'template-parts/mobile-off-canvas' ); ?>
+	<?php endif; ?>
+
+	<?php do_action( 'foundationpress_layout_start' ); ?>
+
+	<header id="masthead" class="site-header" role="banner">
+		<div class="title-bar" data-responsive-toggle="site-navigation">
+			<button class="menu-icon" type="button" data-toggle="mobile-menu"></button>
+			<div class="title-bar-title">
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			</div>
+		</div>
+
+		<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
+			<div class="top-bar-left">
+				<ul class="menu">
+					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+				</ul>
+			</div>
+			<div class="top-bar-right">
+				<?php foundationpress_top_bar_r(); ?>
+
+				<?php if ( ! get_theme_mod( 'wpt_mobile_menu_layout' ) || get_theme_mod( 'wpt_mobile_menu_layout' ) == 'topbar' ) : ?>
+					<?php get_template_part( 'template-parts/mobile-top-bar' ); ?>
+				<?php endif; ?>
+			</div>
+		</nav>
+	</header>
+
+	<section class="container">
+		<?php do_action( 'foundationpress_after_header' ); ?>
+        
+        
+
+        <div class="columns large-7 main-menu">
+            <img class="main-menu-logo" src="<?php echo get_bloginfo('template_directory'); ?>/assets/images/logo.png" scale="0">
+            <ul>
+                <a href="http://www.google.com"><li>About</li></a>
+                <li>Consulting</li>
+                <li>Projects</li>
+                <li>Adventures</li>
+            </ul>
+        </div>
+        <div class="columns large-20 large-offset-7 end content-container">
+            This is a big column
+        </div>
